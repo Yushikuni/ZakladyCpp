87d2d5abfa59e13f99de5e91df121f29acc7ba16
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-04-04 21:53:32 +0200

    Pridani projektu

---
+++ ZakladyCpp/ZakladyCpp.cpp
@@ -0,0 +1,30 @@
+﻿// ZakladyCpp.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+//vezme dve cisla a multiplne je
+int Multiply(int a, int b)
+{
+    std::cout << (a * b) << std::endl;
+    return (a * b);
+}
+
+//vyhodi cislo na screen
+void PrintScreen(int x)
+{
+    std::cout << x << std::endl;
+}
+//hlavni ovladaci cyklus
+int main()
+{
+
+    int Res = Multiply(5, 5);
+    std::cout << "Hello World!\n";
+    std::cout << "Druhy radek kodu!\n";
+
+    PrintScreen(96);
+
+    std::cin.get();
+
+    return 0;
+}
